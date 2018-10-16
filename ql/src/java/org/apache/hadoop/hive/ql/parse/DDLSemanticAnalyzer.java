1f86ee08064173ca8ab41a58ffec0a038455b1a1
Daniel Dai <daijyc@gmail.com>
2018-10-16 13:52:05 -0700

    HIVE-20731: keystore file in JdbcStorageHandler should be authorized (Daniel Dai, reviewed by Thejas Nair)
    Signed-off-by: Thejas M Nair <thejas@hortonworks.com>

--- ql/src/java/org/apache/hadoop/hive/ql/parse/DDLSemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/DDLSemanticAnalyzer.java
@@ -1769,6 +1769,7 @@ public class DDLSemanticAnalyzer extends BaseSemanticAnalyzer {
         alterTblDesc.setDropIfExists(true);
       }
     } else {
+      addPropertyReadEntry(mapProp, inputs);
       alterTblDesc = new AlterTableDesc(AlterTableTypes.ADDPROPS, partSpec, expectView);
     }
     alterTblDesc.setProps(mapProp);
