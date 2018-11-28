a05ce4705e85aea7b33c15a3b173ad658b24ffce
Jesus Camacho Rodriguez <jcamacho@apache.org>
2018-11-27 16:31:36 -0800

    HIVE-20976: JDBC queries containing joins gives wrong results (Jesus Camacho Rodriguez, reviewed by Ashutosh Chauhan)

--- standalone-metastore/src/gen/thrift/gen-php/metastore/Types.php
+++ standalone-metastore/src/gen/thrift/gen-php/metastore/Types.php
@@ -31377,6 +31377,8 @@ final class Constant extends \Thrift\Type\TConstant {
   static protected $TABLE_NO_AUTO_COMPACT;
   static protected $TABLE_TRANSACTIONAL_PROPERTIES;
   static protected $TABLE_BUCKETING_VERSION;
+  static protected $DRUID_CONFIG_PREFIX;
+  static protected $JDBC_CONFIG_PREFIX;
 
   static protected function init_DDL_TIME() {
     return "transient_lastDdlTime";
@@ -31476,6 +31478,14 @@ final class Constant extends \Thrift\Type\TConstant {
 
   static protected function init_TABLE_BUCKETING_VERSION() {
     return "bucketing_version";
+  }
+
+  static protected function init_DRUID_CONFIG_PREFIX() {
+    return "druid.";
+  }
+
+  static protected function init_JDBC_CONFIG_PREFIX() {
+    return "hive.sql.";
   }
 }
 
