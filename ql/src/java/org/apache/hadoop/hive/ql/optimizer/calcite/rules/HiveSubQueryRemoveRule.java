e1ecb67fb29098fef5a89c93b3996b756cba1565
Vineet Garg <vgarg@apache.org>
2019-08-21 15:24:09 -0700

    HIVE-22107: Correlated subquery producing wrong schema (Vineet Garg,reviewed by Ashutosh Chauhan)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveSubQueryRemoveRule.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/rules/HiveSubQueryRemoveRule.java
@@ -216,7 +216,7 @@ public class HiveSubQueryRemoveRule extends RelOptRule {
 
         // id is appended since there could be multiple scalar subqueries and FILTER
         // is created using field name
-        String indicator = "alwaysTrue" + e.rel.getId();
+        String indicator = "trueLiteral";
         parentQueryFields.add(builder.alias(builder.literal(true), indicator));
         builder.project(parentQueryFields);
         builder.join(JoinRelType.LEFT, builder.literal(true), variablesSet);
@@ -361,10 +361,11 @@ public class HiveSubQueryRemoveRule extends RelOptRule {
       }
 
       // Now the left join
+      String trueLiteral = "literalTrue";
       switch (logic) {
       case TRUE:
         if (fields.isEmpty()) {
-          builder.project(builder.alias(builder.literal(true), "i" + e.rel.getId()));
+          builder.project(builder.alias(builder.literal(true), trueLiteral));
           if(!variablesSet.isEmpty()
               && (e.getKind() == SqlKind.EXISTS || e.getKind() == SqlKind.IN)) {
             // avoid adding group by for correlated IN/EXISTS queries
@@ -385,7 +386,7 @@ public class HiveSubQueryRemoveRule extends RelOptRule {
         }
         break;
       default:
-        fields.add(builder.alias(builder.literal(true), "i" + e.rel.getId()));
+        fields.add(builder.alias(builder.literal(true), trueLiteral));
         builder.project(fields);
         builder.distinct();
       }
@@ -425,8 +426,7 @@ public class HiveSubQueryRemoveRule extends RelOptRule {
         operands.add((builder.isNull(builder.field("ct", "c"))), builder.literal(false));
         break;
       }
-      operands.add(builder.isNotNull(builder.field("dt", "i" + e.rel.getId())),
-          builder.literal(true));
+      operands.add(builder.isNotNull(builder.field("dt",trueLiteral)), builder.literal(true));
       if (!keyIsNulls.isEmpty()) {
         //Calcite creates null literal with Null type here but
         // because HIVE doesn't support null type it is appropriately typed boolean
