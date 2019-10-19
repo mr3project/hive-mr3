0e1139b755fb5fa2402d6ed4017d3b07f885a9a7
Marta Kuczora <kuczoram@cloudera.com>
2019-10-18 17:25:55 -0700

    HIVE-22336: Updates should be pushed to the Metastore backend DB before creating the notification event (Marta Kuczora, reviewed by Jesus Camacho Rodriguez)

--- standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ standalone-metastore/src/main/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -9727,6 +9727,7 @@ public class ObjectStore implements RawStore, Configurable {
     boolean commited = false;
     Query query = null;
     try {
+      pm.flush();
       openTransaction();
       lockForUpdate();
       query = pm.newQuery(MNotificationNextId.class);
