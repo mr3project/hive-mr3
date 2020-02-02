5b1758172adc396a2c604569d5d5e5feaba6d34a
Gla Park <glapark@datamonad.com>
2020-02-02 07:34:52 -0800

    preparing MR3 1.0 release

--- ql/src/test/org/apache/hadoop/hive/ql/hooks/TestHiveProtoLoggingHook.java
+++ ql/src/test/org/apache/hadoop/hive/ql/hooks/TestHiveProtoLoggingHook.java
@@ -82,7 +82,7 @@ public class TestHiveProtoLoggingHook {
   @Test
   public void testPreEventLog() throws Exception {
     context.setHookType(HookType.PRE_EXEC_HOOK);
-    EventLogger evtLogger = new EventLogger(conf, SystemClock.getInstance());
+    EventLogger evtLogger = new EventLogger(conf, new SystemClock());
     evtLogger.handle(context);
     evtLogger.shutdown();
 
@@ -113,7 +113,7 @@ public class TestHiveProtoLoggingHook {
     context.getPerfLogger().PerfLogBegin("test", "LogTest");
     context.getPerfLogger().PerfLogEnd("test", "LogTest");
 
-    EventLogger evtLogger = new EventLogger(conf, SystemClock.getInstance());
+    EventLogger evtLogger = new EventLogger(conf, new SystemClock());
     evtLogger.handle(context);
     evtLogger.shutdown();
 
@@ -136,7 +136,7 @@ public class TestHiveProtoLoggingHook {
   public void testFailureEventLog() throws Exception {
     context.setHookType(HookType.ON_FAILURE_HOOK);
 
-    EventLogger evtLogger = new EventLogger(conf, SystemClock.getInstance());
+    EventLogger evtLogger = new EventLogger(conf, new SystemClock());
     evtLogger.handle(context);
     evtLogger.shutdown();
 
@@ -161,7 +161,7 @@ public class TestHiveProtoLoggingHook {
     Assert.assertEquals(1, status.length);
 
     DatePartitionedLogger<HiveHookEventProto> logger = new DatePartitionedLogger<>(
-        HiveHookEventProto.PARSER, path, conf, SystemClock.getInstance());
+        HiveHookEventProto.PARSER, path, conf, new SystemClock());
     ProtoMessageReader<HiveHookEventProto> reader = logger.getReader(status[0].getPath());
     HiveHookEventProto event = reader.readEvent();
     Assert.assertNotNull(event);
--- shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
+++ shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
@@ -1127,11 +1127,10 @@ public class Hadoop23Shims extends HadoopShimsSecure {
 
   @Override
   public boolean runDistCp(List<Path> srcPaths, Path dst, Configuration conf) throws IOException {
-       DistCpOptions options = new DistCpOptions.Builder(srcPaths, dst)
-        .withSyncFolder(true)
-        .withCRC(true)
-        .preserve(FileAttribute.BLOCKSIZE)
-        .build();
+    DistCpOptions options = new DistCpOptions(srcPaths, dst);
+    options.setSyncFolder(true);
+    options.setSkipCRC(true);
+    options.preserve(FileAttribute.BLOCKSIZE);
 
     // Creates the command-line parameters for distcp
     List<String> params = constructDistCpParams(srcPaths, dst, conf);
