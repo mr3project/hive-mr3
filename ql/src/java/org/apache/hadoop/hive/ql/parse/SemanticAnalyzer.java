2e62c26d801f3b22aaece3f9c3b62aabda24e3b8
Slim Bouguerra <bslim@apache.org>
2020-02-28 09:23:03 -0800

    HIVE-22629: Avoid Array resizing/recreation  by using the construcor/ref instead of iteration/get_i (Slim Bouguerra reviewed by Vineet Garg)

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -13766,8 +13766,9 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
         }
       }
 
-      for (int i = next.getChildren().size() - 1; i >= 0; i--) {
-        stack.push((ASTNode)next.getChildren().get(i));
+      ArrayList childrenList = next.getChildren();
+      for (int i = childrenList.size() - 1; i >= 0; i--) {
+        stack.push((ASTNode)childrenList.get(i));
       }
     }
   }
