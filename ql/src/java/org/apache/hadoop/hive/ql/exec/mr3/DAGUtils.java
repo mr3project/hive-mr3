035175859db7ad8826cf928183a3f67cf4c36dc5
Sungwoo Park <glapark@datamonad.com>
2020-04-06 19:14:47 -0700

    apply HIVE-18871 to DAGUtils: hive on tez execution error due to set hive.aux.jars.path to hdfs://

--- ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/DAGUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/DAGUtils.java
@@ -27,6 +27,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.hive.common.FileUtils;
@@ -1312,7 +1313,12 @@ public class DAGUtils {
         return createLocalResource(destFS, dest, type, LocalResourceVisibility.PRIVATE);
       }
       try {
-        destFS.copyFromLocalFile(false, false, src, dest);
+        if (src.toUri().getScheme()!=null) {
+          FileUtil.copy(src.getFileSystem(conf), src, destFS, dest, false, false, conf);
+        }
+        else {
+          destFS.copyFromLocalFile(false, false, src, dest);
+        }
         synchronized (notifier) {
           notifier.notifyAll(); // Notify if we have successfully copied the file.
         }
