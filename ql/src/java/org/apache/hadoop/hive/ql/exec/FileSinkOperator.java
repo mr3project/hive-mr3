difin/difin_hive

HIVE-22941 : Empty files are inserted into external tables after HIVE-21714 - workaround (Laszlo Bodor via Ashutosh Chauhan)
Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
@@ -1336,7 +1336,8 @@ public class FileSinkOperator extends TerminalOperator<FileSinkDesc> implements
       Class<?> clazz = conf.getTableInfo().getOutputFileFormatClass();
       boolean isStreaming = StreamingOutputFormat.class.isAssignableFrom(clazz);
 
-      if (!isTez || isStreaming || this.isInsertOverwrite) {
+      // let empty file generation for mm/acid table as a quick and dirty workaround for HIVE-22941
+      if (!isTez || isStreaming || (this.isInsertOverwrite && (conf.isMmTable() || conf.isFullAcidTable()))) {
         createBucketFiles(fsp);
       }
     }

mr3project/hive-mr3

ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
@@ -1335,8 +1335,9 @@ public class FileSinkOperator extends TerminalOperator<FileSinkDesc> implements
       boolean isMr3 = engine.equalsIgnoreCase("mr3") || engine.equalsIgnoreCase("tez");
       Class<?> clazz = conf.getTableInfo().getOutputFileFormatClass();
       boolean isStreaming = StreamingOutputFormat.class.isAssignableFrom(clazz);
-      
-      if (!isMr3 || isStreaming || this.isInsertOverwrite) {
+
+      // let empty file generation for mm/acid table as a quick and dirty workaround for HIVE-22941
+      if (!isMr3 || isStreaming || (this.isInsertOverwrite && (conf.isMmTable() || conf.isFullAcidTable()))) {
         createBucketFiles(fsp);
       }
     }