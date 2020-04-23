83bff2608844e2640f9823f3d8ef4518d0a44b25
Sungwoo Park <glapark@datamonad.com>
2020-04-23 10:00:31 -0700

    compile okay: add DriverContext in MR3Task

--- ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/MR3Task.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/MR3Task.java
@@ -24,6 +24,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.DriverContext;
 import org.apache.hadoop.hive.ql.Context;
 import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.exec.Utilities;
