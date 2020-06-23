e027e8792eb0cb1487d9a8eccede43d79e2145b4
Sungwoo Park <glapark@datamonad.com>
2020-06-23 07:23:04 -0700

    use LOG.debug: Reading ORC rows from...

--- ql/src/java/org/apache/hadoop/hive/ql/io/orc/ReaderImpl.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/orc/ReaderImpl.java
@@ -76,7 +76,10 @@ public class ReaderImpl extends org.apache.orc.impl.ReaderImpl
 
   @Override
   public RecordReader rowsOptions(Options options) throws IOException {
-    LOG.info("Reading ORC rows from " + path + " with " + options);
+    LOG.info("Reading ORC rows from " + path);
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("with " + options);
+    }
     return new RecordReaderImpl(this, options);
   }
 
