23e9f4a0539f42f33e5f950eaaf99d9f29254f19
belugabehr <12578579+belugabehr@users.noreply.github.com>
2021-02-23 08:45:20 -0500

    HIVE-24808: Cache Parsed Dates (David Mollitor reviewed by Miklos Gergely)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/TruncDateFromTimestamp.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/TruncDateFromTimestamp.java
@@ -12,7 +12,6 @@ import org.apache.hadoop.hive.ql.exec.vector.VectorExpressionDescriptor.Argument
 import org.apache.hadoop.hive.ql.exec.vector.VectorExpressionDescriptor.Descriptor;
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hive.common.util.DateParser;
 
 /**
  * Vectorized implementation of trunc(date, fmt) function for timestamp input
@@ -21,7 +20,6 @@ public class TruncDateFromTimestamp extends VectorExpression {
   private static final long serialVersionUID = 1L;
   protected int colNum;
   protected String fmt;
-  protected transient final DateParser dateParser = new DateParser();
 
   public TruncDateFromTimestamp() {
     super();
