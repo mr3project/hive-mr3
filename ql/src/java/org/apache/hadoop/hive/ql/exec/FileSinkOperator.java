c2b53f14b5f74bab31e60143280fe2f3c2988fb6
Sungwoo Park <glapark@datamonad.com>
2021-03-16 02:00:34 -0700

    HIVE-19326.extra

--- ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
@@ -32,6 +32,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.function.BiFunction;
 
 import com.google.common.collect.Lists;
 
