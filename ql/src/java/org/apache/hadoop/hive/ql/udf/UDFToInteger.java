6c2a477cbd59e4d7c6a1865da5c6937ebbf2d7f0
Adesh Kumar Rao <adrao@microsoft.com>
2021-07-08 15:11:31 +0530

    HIVE-25299: Casting timestamp to numeric data types is incorrect for non-UTC timezones (Adesh Rao, reviewed by Ashish Kumar, Sankar Hariappan)
    Signed-off-by: Sankar Hariappan <sankarh@apache.org>
    Closes (#2447)

--- ql/src/java/org/apache/hadoop/hive/ql/udf/UDFToInteger.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/UDFToInteger.java
@@ -197,7 +197,7 @@ public class UDFToInteger extends UDF {
     if (i == null) {
       return null;
     } else {
-      final long longValue = i.getSeconds();
+      final long longValue = UDFUtils.getTimestampTZFromTimestamp(i.getTimestamp()).getEpochSecond();
       final int intValue = (int) longValue;
       if (intValue != longValue) {
         return null;
