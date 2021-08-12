difin/difin_hive

HIVE-22948: QueryCache: Treat query cache locations as temporary storage (Gopal V, reviewed by Vineet Garg)
Signed-off-by: Gopal V <gopalv@apache.org>

ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -7767,7 +7767,9 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       }
 
       boolean isDestTempFile = true;
-      if (!ctx.isMRTmpFileURI(destinationPath.toUri().toString())) {
+      if (ctx.isMRTmpFileURI(destinationPath.toUri().toString()) == false
+          && ctx.isResultCacheDir(destinationPath) == false) {
+        // not a temp dir and not a result cache dir
         idToTableNameMap.put(String.valueOf(destTableId), destinationPath.toUri().toString());
         currentTableId = destTableId;
         destTableId++;

mr3project/hive-mr3

ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -7576,7 +7576,9 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       destTableIsFullAcid = tblDesc != null && AcidUtils.isFullAcidTable(tblDesc);
 
       boolean isDestTempFile = true;
-      if (!ctx.isMRTmpFileURI(dest_path.toUri().toString())) {
+      if (ctx.isMRTmpFileURI(dest_path.toUri().toString()) == false
+          && ctx.isResultCacheDir(dest_path) == false) {
+        // not a temp dir and not a result cache dir
         idToTableNameMap.put(String.valueOf(destTableId), dest_path.toUri().toString());
         currentTableId = destTableId;
         destTableId++;