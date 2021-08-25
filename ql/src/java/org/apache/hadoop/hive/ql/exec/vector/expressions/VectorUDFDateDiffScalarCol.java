6155c6db47df4b969029e18d0ec6a48f559ff79e
Shubham Chaurasia <shubh.chaurasia@gmail.com>
2021-08-25 12:58:11 +0530

    HIVE-25449: datediff() gives wrong output when run in a tez task with some non-UTC timezone (#2591) (Shubham Chaurasia reviewed by Laszlo Bodor)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/VectorUDFDateDiffScalarCol.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/VectorUDFDateDiffScalarCol.java
@@ -30,10 +30,9 @@ import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector.Pr
 import org.apache.hadoop.hive.serde2.typeinfo.PrimitiveTypeInfo;
 import org.apache.hadoop.io.Text;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 public class VectorUDFDateDiffScalarCol extends VectorExpression {
@@ -45,7 +44,6 @@ public class VectorUDFDateDiffScalarCol extends VectorExpression {
   private Timestamp timestampValue = null;
   private byte[] stringValue;
 
-  private transient final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
   private transient final Text text = new Text();
   private transient final Date date = new Date(0);
 
@@ -112,8 +110,9 @@ public class VectorUDFDateDiffScalarCol extends VectorExpression {
       case CHAR:
       case VARCHAR:
         try {
-          date.setTime(formatter.parse(new String(stringValue, "UTF-8")).getTime());
-          baseDate = DateWritableV2.dateToDays(date);
+          org.apache.hadoop.hive.common.type.Date hiveDate
+              = org.apache.hadoop.hive.common.type.Date.valueOf(new String(stringValue, StandardCharsets.UTF_8));
+          baseDate = hiveDate.toEpochDay();
           break;
         } catch (Exception e) {
           outputColVector.noNulls = false;
@@ -358,9 +357,10 @@ public class VectorUDFDateDiffScalarCol extends VectorExpression {
     BytesColumnVector bcv = (BytesColumnVector) columnVector;
     text.set(bcv.vector[i], bcv.start[i], bcv.length[i]);
     try {
-      date.setTime(formatter.parse(text.toString()).getTime());
-      output.vector[i] = baseDate - DateWritableV2.dateToDays(date);
-    } catch (ParseException e) {
+      org.apache.hadoop.hive.common.type.Date hiveDate
+          = org.apache.hadoop.hive.common.type.Date.valueOf(text.toString());
+      output.vector[i] = baseDate - hiveDate.toEpochDay();
+    } catch (IllegalArgumentException e) {
       output.vector[i] = 1;
       output.isNull[i] = true;
     }
