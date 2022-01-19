difin/difin_hive

HIVE-20486: Vectorization support for Kafka Storage Handler (Slim Bouguerra, reviewd by V. Gopal)

itests/util/src/main/java/org/apache/hadoop/hive/ql/QTestUtil.java
@@ -627,7 +627,9 @@ public class QTestUtil {
       druidCluster.start();
     }
 
-    if(clusterType == MiniClusterType.kafka || clusterType == MiniClusterType.druidKafka) {
+    if (clusterType == MiniClusterType.kafka
+        || clusterType == MiniClusterType.druidKafka
+        || clusterType == MiniClusterType.druidLocal) {
       kafkaCluster = new SingleNodeKafkaCluster("kafka",
           logDir + "/kafka-cluster",
           setup.zkPort

mr3project/hive-mr3

itests/util/src/main/java/org/apache/hadoop/hive/ql/QTestUtil.java
@@ -717,7 +717,8 @@ public class QTestUtil {
       druidCluster.start();
     }
 
-    if(clusterType == MiniClusterType.kafka || clusterType == MiniClusterType.druidKafka) {
+    if (clusterType == MiniClusterType.kafka
+        || clusterType == MiniClusterType.druidKafka) {
       kafkaCluster = new SingleNodeKafkaCluster("kafka",
           getLogDirectory() + "/kafka-cluster",
           setup.zkPort