0a80b7d712cf0d03acce82d453e4e3ab268e2281
Sungwoo Park <glapark@datamonad.com>
2022-10-06 07:48:30 -0700

    tez.Utils.getSplitLocationProvider() uses SplitLocationProvider for MR3

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/Utils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/Utils.java
@@ -35,6 +35,11 @@ import org.apache.hadoop.mapred.split.SplitLocationProvider;
 import org.apache.tez.common.counters.TezCounters;
 import org.slf4j.Logger;
 
+import org.apache.hadoop.hive.serde2.SerDeUtils;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hive.common.util.Murmur3;
+import org.apache.tez.runtime.api.InputInitializerContext;
+
 public class Utils {
 
   public static SplitLocationProvider getSplitLocationProvider(Configuration conf, Logger LOG)
@@ -43,12 +48,22 @@ public class Utils {
     return getSplitLocationProvider(conf, true, LOG);
   }
 
-  public static SplitLocationProvider getSplitLocationProvider(Configuration conf, boolean useCacheAffinity, Logger LOG) throws
-      IOException {
+  public static SplitLocationProvider getSplitLocationProvider(Configuration conf,
+                                                               boolean useCacheAffinity,
+                                                               Logger LOG) throws IOException {
+    return getSplitLocationProvider(conf, useCacheAffinity, null, LOG);
+  }
+
+  public static SplitLocationProvider getSplitLocationProvider(Configuration conf,
+                                                               boolean useCacheAffinity,
+                                                               InputInitializerContext context,
+                                                               Logger LOG) throws IOException {
     boolean useCustomLocations =
         HiveConf.getVar(conf, HiveConf.ConfVars.HIVE_EXECUTION_MODE).equals("llap")
         && HiveConf.getBoolVar(conf, HiveConf.ConfVars.LLAP_CLIENT_CONSISTENT_SPLITS) 
-        && useCacheAffinity;
+        && useCacheAffinity
+        && HiveConf.getVar(conf, HiveConf.ConfVars.HIVE_EXECUTION_ENGINE).equals("tez")
+        && context != null;
     SplitLocationProvider splitLocationProvider;
     final String locationProviderClass = HiveConf.getVar(conf, HiveConf.ConfVars.LLAP_SPLIT_LOCATION_PROVIDER_CLASS);
     final boolean customLocationProvider =
@@ -73,8 +88,45 @@ public class Utils {
       }
       return locationProviderImpl;
     } else if (useCustomLocations) {
-      LlapRegistryService serviceRegistry = LlapRegistryService.getClient(conf);
-      return getCustomSplitLocationProvider(serviceRegistry, LOG);
+      splitLocationProvider = new SplitLocationProvider() {
+        @Override
+        public String[] getLocations(InputSplit split) throws IOException {
+          if (!(split instanceof FileSplit)) {
+            return split.getLocations();
+          }
+          FileSplit fsplit = (FileSplit) split;
+          long hash = hash1(getHashInputForSplit(fsplit.getPath().toString(), fsplit.getStart()));
+          String location = context.getLocationHintFromHash(hash);
+          if (LOG.isDebugEnabled()) {
+            String splitDesc = "Split at " + fsplit.getPath() + " with offset=" + fsplit.getStart();
+            LOG.debug(splitDesc + " mapped to location=" + location);
+          }
+          return (location != null) ? new String[] { location } : null;
+        }
+
+        private byte[] getHashInputForSplit(String path, long start) {
+          // Explicitly using only the start offset of a split, and not the length. Splits generated on
+          // block boundaries and stripe boundaries can vary slightly. Try hashing both to the same node.
+          // There is the drawback of potentially hashing the same data on multiple nodes though, when a
+          // large split is sent to 1 node, and a second invocation uses smaller chunks of the previous
+          // large split and send them to different nodes.
+          byte[] pathBytes = path.getBytes();
+          byte[] allBytes = new byte[pathBytes.length + 8];
+          System.arraycopy(pathBytes, 0, allBytes, 0, pathBytes.length);
+          SerDeUtils.writeLong(allBytes, pathBytes.length, start >> 3);
+          return allBytes;
+         }
+
+        private long hash1(byte[] bytes) {
+          final int PRIME = 104729; // Same as hash64's default seed.
+          return Murmur3.hash64(bytes, 0, bytes.length, PRIME);
+        }
+
+        @Override
+        public String toString() {
+          return "LLAP SplitLocationProvider";
+        }
+      };
     } else {
       splitLocationProvider = new SplitLocationProvider() {
         @Override
