36caa53ef1d7fab9a4b1d99c561b02cae67f5639
Sungwoo Park <glapark@datamonad.com>
2022-12-20 04:10:53 -0800

    do not call progressBarCompleted() if progressBar exists

--- jdbc/src/java/org/apache/hive/jdbc/HiveStatement.java
+++ jdbc/src/java/org/apache/hive/jdbc/HiveStatement.java
@@ -424,7 +424,7 @@ public class HiveStatement implements java.sql.Statement {
     statusReq.setGetProgressUpdate(inPlaceUpdateStream.isPresent());
 
     // Progress bar is completed if there is nothing to request
-    if (inPlaceUpdateStream.isPresent()) {
+    if (!inPlaceUpdateStream.isPresent()) {
       inPlaceUpdateStream.get().getEventNotifier().progressBarCompleted();
     }
 
