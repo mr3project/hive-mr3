difin/difin_hive

HIVE-20009 : Fix runtime stats for merge statement (Zoltan Haindrich via Ashutosh Chauhan)
Signed-off-by: Ashutosh Chauhan <hashutosh@apache.org>

ql/src/test/queries/clientpositive/runtime_stats_merge.q
@@ -1,0 +1,41 @@
+
+set hive.mapred.mode=nonstrict;
+set hive.security.authorization.manager=org.apache.hadoop.hive.ql.security.authorization.plugin.sqlstd.SQLStdHiveAuthorizerFactoryForTest;
+set hive.support.concurrency=true;
+set hive.explain.user=true;
+set hive.txn.manager=org.apache.hadoop.hive.ql.lockmgr.DbTxnManager;
+
+
+set hive.auto.convert.join=true;
+set hive.auto.convert.join.noconditionaltask=true;
+set hive.auto.convert.join.noconditionaltask.size=88888888;
+-- set hive.auto.convert.sortmerge.join=true;
+-- set hive.auto.convert.sortmerge.join.to.mapjoin=true;
+
+create table lineitem (L_ORDERKEY integer);
+
+insert into lineitem values (1),(2),(3);
+
+create table lineitem2
+ stored as orc  TBLPROPERTIES ('transactional'='true')
+  as select * from lineitem;
+create table lineitem_stage
+ stored as orc  TBLPROPERTIES ('transactional'='true')
+  as select * from lineitem limit 1;
+
+
+analyze table lineitem2 compute statistics for columns;
+analyze table lineitem_stage compute statistics for columns;
+
+explain reoptimization
+merge into lineitem2 using
+	(select * from lineitem_stage) sub
+	on sub.L_ORDERKEY = lineitem2.L_ORDERKEY
+	when matched then delete;
+
+merge into lineitem2 using
+	(select * from lineitem_stage) sub
+	on sub.L_ORDERKEY = lineitem2.L_ORDERKEY
+	when matched then delete;
+
+	

mr3project/hive-mr3

(cherry picked from commit 78cbf147873752e7955fff37416edba372e2b69a)
Adapted to branch-3: minor q.out changes

ql/src/test/queries/clientpositive/runtime_stats_merge.q
@@ -1,0 +1,41 @@
+
+set hive.mapred.mode=nonstrict;
+set hive.security.authorization.manager=org.apache.hadoop.hive.ql.security.authorization.plugin.sqlstd.SQLStdHiveAuthorizerFactoryForTest;
+set hive.support.concurrency=true;
+set hive.explain.user=true;
+set hive.txn.manager=org.apache.hadoop.hive.ql.lockmgr.DbTxnManager;
+
+
+set hive.auto.convert.join=true;
+set hive.auto.convert.join.noconditionaltask=true;
+set hive.auto.convert.join.noconditionaltask.size=88888888;
+-- set hive.auto.convert.sortmerge.join=true;
+-- set hive.auto.convert.sortmerge.join.to.mapjoin=true;
+
+create table lineitem1 (L_ORDERKEY integer);
+
+insert into lineitem1 values (1),(2),(3);
+
+create table lineitem2
+ stored as orc  TBLPROPERTIES ('transactional'='true')
+  as select * from lineitem1;
+create table lineitem_stage
+ stored as orc  TBLPROPERTIES ('transactional'='true')
+  as select * from lineitem1 limit 1;
+
+
+analyze table lineitem2 compute statistics for columns;
+analyze table lineitem_stage compute statistics for columns;
+
+explain reoptimization
+merge into lineitem2 using
+	(select * from lineitem_stage) sub
+	on sub.L_ORDERKEY = lineitem2.L_ORDERKEY
+	when matched then delete;
+
+merge into lineitem2 using
+	(select * from lineitem_stage) sub
+	on sub.L_ORDERKEY = lineitem2.L_ORDERKEY
+	when matched then delete;
+
+	