19effbd967d02f72d3d2978b18369928038ede34
Gla Park <glapark@datamonad.com>
2024-03-27 01:55:09 -0700

    Minor: Comment on ObjectCacheFactory.getPerTaskMrCache() in MergeFileRecordProcessor

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/MergeFileRecordProcessor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/MergeFileRecordProcessor.java
@@ -94,6 +94,7 @@ public class MergeFileRecordProcessor extends RecordProcessor {
     }
 
     String queryId = HiveConf.getVar(jconf, HiveConf.ConfVars.HIVE_QUERY_ID);
+    // do not consider ObjectCacheFactory.getPerTaskMrCache() because there is no MergeWork
     cache = ObjectCacheFactory.getCache(jconf, queryId, true);
 
     try {
