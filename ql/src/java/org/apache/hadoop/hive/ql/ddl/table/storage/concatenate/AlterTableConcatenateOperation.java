656dc69c27bdb0c052c8fc8dfbe94aa682b1a1b7
Sungwoo Park <glapark@datamonad.com>
2024-04-20 01:42:40 -0700

    AlterTableConcatenateOperation disables speculative execution in MergeFileTask.

--- ql/src/java/org/apache/hadoop/hive/ql/ddl/table/storage/concatenate/AlterTableConcatenateOperation.java
+++ ql/src/java/org/apache/hadoop/hive/ql/ddl/table/storage/concatenate/AlterTableConcatenateOperation.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.ql.CompilationOpContext;
 import org.apache.hadoop.hive.ql.Context;
@@ -127,8 +128,20 @@ public class AlterTableConcatenateOperation extends DDLOperation<AlterTableConca
 
   private int executeTask(Context generalContext, Task<?> task) {
     TaskQueue taskQueue = new TaskQueue();
+
+    // Disable speculative execution
+    HiveConf hiveConf = context.getQueryState().getConf();
+    int concurrentRunThreshold = hiveConf.getIntVar(HiveConf.ConfVars.MR3_AM_TASK_CONCURRENT_RUN_THRESHOLD_PERCENT);
+    if (concurrentRunThreshold != 100) {
+      LOG.info("Disable speculative execution for MergeFileTask: {}", concurrentRunThreshold);
+      hiveConf.setIntVar(HiveConf.ConfVars.MR3_AM_TASK_CONCURRENT_RUN_THRESHOLD_PERCENT, 100);
+    }
+
     task.initialize(context.getQueryState(), context.getQueryPlan(), taskQueue, generalContext);
     int ret = task.execute();
+
+    // TODO: Restore speculative execution if necessary (when context.getQueryState().getConf() is shared)
+
     if (task.getException() != null) {
       context.getTask().setException(task.getException());
     }
