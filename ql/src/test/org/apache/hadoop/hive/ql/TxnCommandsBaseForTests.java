e72925b4e1fb215bccfd6f34762248bcde90df0c
seonggon <lngsg@postech.ac.kr>
2024-07-17 13:20:18 +0900

    Fix unit tests in ql
    TxnCommandsBaseForTests: shutdown MR3Client after each test
    TestOperators#testHashGroupBy: Set estimateNumExecutors to 1 just like MR engine

--- ql/src/test/org/apache/hadoop/hive/ql/TxnCommandsBaseForTests.java
+++ ql/src/test/org/apache/hadoop/hive/ql/TxnCommandsBaseForTests.java
@@ -41,6 +41,7 @@ import org.apache.hadoop.hive.metastore.conf.MetastoreConf;
 import org.apache.hadoop.hive.metastore.utils.TestTxnDbUtil;
 import org.apache.hadoop.hive.metastore.txn.TxnStore;
 import org.apache.hadoop.hive.metastore.txn.TxnUtils;
+import org.apache.hadoop.hive.ql.exec.mr3.session.MR3SessionManagerImpl;
 import org.apache.hadoop.hive.ql.io.HiveInputFormat;
 import org.apache.hadoop.hive.ql.metadata.HiveMetaStoreClientWithLocalCache;
 import org.apache.hadoop.hive.ql.processors.CommandProcessorException;
@@ -177,6 +178,7 @@ public abstract class TxnCommandsBaseForTests {
         d.close();
         d.destroy();
         d = null;
+        MR3SessionManagerImpl.getInstance().shutdown();
       }
     } finally {
       TestTxnDbUtil.cleanDb(hiveConf);
