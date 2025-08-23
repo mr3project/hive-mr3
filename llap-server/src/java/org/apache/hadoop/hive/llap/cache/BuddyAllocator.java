bc090e4f8d69c19420f441d73a28834342742ad4
Sungwoo Park <glapark@datamonad.com>
2025-08-23 02:27:36 -0700

    Experimental: support using Java heap memory for LLAP/IO cache.
    To use Java heap memory for LLAP/IO cache, set the following configuration keys:
    
    1. hive.llap.io.enabled to true
    2. hive.mr3.llap.use.heap.memory to true
    3. hive.mr3.llap.heap.memory.max.size.mb to the maximum size of LLAP/IO cache in MB
    4. hive.llap.io.allocator.direct to false (because we use Java heap memory)
    5. hive.llap.io.memory.size to 0 (because we do not use native memory)
    6. hive.mr3.llap.headroom.mb to 0 (because we do not use native memory)
    7. hive.llap.io.threadpool.size
    8. hive.llap.daemon.num.executors
    
    To use native memory for LLAP/IO cache, set hive.mr3.llap.use.heap.memory to false.

--- llap-server/src/java/org/apache/hadoop/hive/llap/cache/BuddyAllocator.java
+++ llap-server/src/java/org/apache/hadoop/hive/llap/cache/BuddyAllocator.java
@@ -51,7 +51,6 @@ import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.llap.io.api.impl.LlapIoImpl;
 import org.apache.hadoop.hive.llap.metrics.LlapDaemonCacheMetrics;
-import org.apache.hadoop.hive.ql.io.orc.encoded.StoppableAllocator;
 
 /**
  *
@@ -72,7 +71,7 @@ import org.apache.hadoop.hive.ql.io.orc.encoded.StoppableAllocator;
  *       Each Buddy Allocator tree can be split up to chunks of size {@link ConfVars#LLAP_ALLOCATOR_MIN_ALLOC} 4KB
  */
 public final class BuddyAllocator
-    implements EvictionAwareAllocator, StoppableAllocator, BuddyAllocatorMXBean, LlapIoDebugDump {
+    implements EvictionAwareStoppableAllocator, BuddyAllocatorMXBean {
   private static final String FAILED_TO_ALLOCATE_MSG =
       "Failed to allocate [{}]X[{}] bytes after [{}] attempt, evicted [{}] bytes and partially allocated [{}] bytes";
   private final Arena[] arenas;
