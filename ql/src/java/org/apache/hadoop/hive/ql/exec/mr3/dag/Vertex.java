b160f0e4dd52ae01058afc375d60471d427c45d4
Sungwoo Park <glapark@datamonad.com>
2026-01-15 23:34:15 -0800

    Set WorkerVertex.hasPerVertexCache

--- ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/dag/Vertex.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/mr3/dag/Vertex.java
@@ -67,6 +67,8 @@ public class Vertex {
   private int distanceFromRoot = -1;  // not calculated yet
   private int hasReducerFromRoot = -1;  // -1 == unknown, 0 == false, 1 == true
 
+  private boolean hasPerVertexCache = false;
+
   private Vertex(
       String name,
       EntityDescriptor processorDescriptor,
@@ -249,6 +251,7 @@ public class Vertex {
         .setResource(createResourceProto(vcoresDivisor))
         // do not set UniquePerNode
         .setPriority(getDistanceFromRoot() * 3)
+        .setHasPerVertexCache(hasPerVertexCache)
         .addAllInEdgeIds(Lists.transform(inputEdges, transformEdgeToIdFunc))
         .addAllOutEdgeIds(Lists.transform(outputEdges, transformEdgeToIdFunc))
         .addAllRootInputs(createRootInputProtos())
@@ -322,5 +325,13 @@ public class Vertex {
 
   public ByteString getProcessorDescriptorPayload() {
     return processorDescriptor.getUserPayload();
+  }
+
+  public Map<String, Pair<EntityDescriptor, EntityDescriptor>> getDataSinks() {
+    return dataSinks;
+  }
+
+  public void setHasPerVertexCache() {
+    hasPerVertexCache = true;
   }
 }
