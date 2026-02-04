f2934261d593cb857f43b8c0c0782c700ec042a5
Sungwoo Park <glapark@datamonad.com>
2026-02-04 17:09:40 +0900

    Call ProcessorContext.notifyPerVertexCacheLoaded() at the end of RecordProcessor.init() because a task may have multiple per-vertex caches

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/ReduceRecordProcessor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/ReduceRecordProcessor.java
@@ -235,6 +235,7 @@ public class ReduceRecordProcessor extends RecordProcessor {
       reducer.setReporter(reporter);
       MapredContext.get().setReporter(reporter);
 
+      processorContext.notifyPerVertexCacheLoaded();
     } catch (Throwable e) {
       super.setAborted(true);
       if (e instanceof OutOfMemoryError) {
