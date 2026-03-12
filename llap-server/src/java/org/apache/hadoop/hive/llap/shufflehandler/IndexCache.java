9ec70e57f1063fb60a867707b16e6cb8b44537f4
Sungwoo Park <glapark@datamonad.com>
2026-03-13 01:42:29 +0900

    Use a valid TezSpillRecord constructor

--- llap-server/src/java/org/apache/hadoop/hive/llap/shufflehandler/IndexCache.java
+++ llap-server/src/java/org/apache/hadoop/hive/llap/shufflehandler/IndexCache.java
@@ -118,7 +118,7 @@ class IndexCache {
     LOG.debug("IndexCache MISS: MapId " + mapId + " not found") ;
     TezSpillRecord tmp = null;
     try {
-      tmp = new TezSpillRecord(indexFileName, null, expectedIndexOwner);  // use null for FileSystem (Cf. TEZ-4145) and because we do not use LLAP ShuffleHandler
+      tmp = new TezSpillRecord(indexFileName, null); // use null for FileSystem (Cf. TEZ-4145) and because we do not use LLAP ShuffleHandler
     } catch (Throwable e) {
       tmp = new TezSpillRecord(0);
       cache.remove(mapId);
