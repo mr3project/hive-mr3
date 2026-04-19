bf52a1b71f569e99d616c405efb1206e36364d23
Sungwoo Park <glapark@datamonad.com>
2026-04-20 00:21:02 +0900

    FastByteComparisons in org.apache.tez.util

--- storage-api/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/StringExpr.java
+++ storage-api/src/java/org/apache/hadoop/hive/ql/exec/vector/expressions/StringExpr.java
@@ -23,7 +23,7 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 
 import org.apache.hadoop.hive.ql.exec.vector.BytesColumnVector;
-import org.apache.tez.runtime.library.utils.FastByteComparisons;
+import org.apache.tez.util.FastByteComparisons;
 
 /**
  * String expression evaluation helper functions.
