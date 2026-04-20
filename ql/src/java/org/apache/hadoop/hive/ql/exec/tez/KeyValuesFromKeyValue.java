9e649470311dfd4c64bf22a106ec8c33692c585d
Sungwoo Park <glapark@datamonad.com>
2026-04-21 00:18:17 +0900

    Use zero-copy BytesWritable in VectorMapJoinFastHashTableLoader

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/KeyValuesFromKeyValue.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/KeyValuesFromKeyValue.java
@@ -72,11 +72,13 @@ public class KeyValuesFromKeyValue implements KeyValuesAdapter {
     this.reader = reader;
   }
 
+  // The backing byte[] array of BytesWritable is immutable, so the consumer may keep pointers to it.
   @Override
   public BytesWritable getCurrentKey() throws IOException {
     return reader.getCurrentKey();
   }
 
+  // The backing byte[] array of BytesWritable is immutable, so the consumer may keep pointers to it.
   @Override
   public Iterable<BytesWritable> getCurrentValues() throws IOException {
     BytesWritable obj = reader.getCurrentValue();
