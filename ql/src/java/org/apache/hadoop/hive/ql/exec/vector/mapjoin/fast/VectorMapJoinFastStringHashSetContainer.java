f996714c6f2e103a388a63ee30cf538ff704d680
Sungwoo Park <glapark@datamonad.com>
2026-04-21 00:44:33 +0900

    VectorMapJoinFastHashTableLoader: getHashCode() uses zero-copy

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/mapjoin/fast/VectorMapJoinFastStringHashSetContainer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/mapjoin/fast/VectorMapJoinFastStringHashSetContainer.java
@@ -74,9 +74,10 @@ public class VectorMapJoinFastStringHashSetContainer extends VectorMapJoinFastHa
 
   @Override
   public long getHashCode(BytesWritable currentKey) throws HiveException, IOException {
-    byte[] keyBytes = currentKey.getBytes();
+    byte[] keyBytes = currentKey.getBytesRaw();
+    int keyOffset = currentKey.getOffset();
     int keyLength = currentKey.getLength();
-    keyBinarySortableDeserializeRead.set(keyBytes, 0, keyLength);
+    keyBinarySortableDeserializeRead.set(keyBytes, keyOffset, keyLength);
     try {
       if (!keyBinarySortableDeserializeRead.readNextField()) {
         return 0;
