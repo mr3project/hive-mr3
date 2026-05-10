9931aa9717a0461c6633819d74748f821e60bc3a
Sungwoo Park <glapark@datamonad.com>
2026-05-10 22:34:58 +0900

    Remove TezConfigurationFactory.wrapWithJobConf()/addProgrammaticallyAddedTezOptsToDagConf(). Use TezConfigurationFactory.wrapWithJobConfWithoutSourceTracking().

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/TezTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/TezTask.java
@@ -544,7 +544,6 @@ public class TezTask extends Task<TezWork> {
 
     LOG.debug("DagInfo: {}", dagInfo);
 
-    TezConfigurationFactory.addProgrammaticallyAddedTezOptsToDagConf(dag.getDagConf(), conf);
     dag.setDAGInfo(dagInfo);
 
     dag.setCredentials(conf.getCredentials());
