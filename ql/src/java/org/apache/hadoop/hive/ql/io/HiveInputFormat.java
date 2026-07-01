3460f0001c4cd85e3ee746df89b1cbbf0b77f65e
mr3project <59910162+mr3project@users.noreply.github.com>
2026-07-01 23:52:17 +0900

    Update Hive input format handling for MR3
    Only wrap input formats with LLAP IO when MR3 has scheduled the task on a
    host that can use the cache, while keeping the existing LLAP enablement
    and input-format support checks.
    
    Use the MR3-aware limit check for Hive input splits and allow thread
    attempt ids to be cleaned separately from IOContext removal.

--- ql/src/java/org/apache/hadoop/hive/ql/io/HiveInputFormat.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/HiveInputFormat.java
@@ -75,6 +75,7 @@ import org.apache.hadoop.util.StringUtils;
 import org.apache.hive.common.util.HiveStringUtils;
 import org.apache.hive.common.util.Ref;
 import org.apache.hive.common.util.ReflectionUtil;
+import org.apache.tez.mapreduce.input.MRInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -295,6 +296,11 @@ public class HiveInputFormat<K extends WritableComparable, V extends Writable>
     if (!HiveConf.getBoolVar(conf, ConfVars.LLAP_IO_ENABLED, LlapProxy.isDaemon())) {
       return inputFormat; // LLAP not enabled, no-op.
     }
+
+    if (!conf.getBoolean(MRInput.TEZ_MR3_SCHEDULED_ON_HOST, false)) {
+      return inputFormat; // use LLAP-IO cache only if TEZ_MR3_SCHEDULED_ON_HOST is exlicitly set
+    }
+
     String ifName = inputFormat.getClass().getCanonicalName();
     boolean isSupported = inputFormat instanceof LlapWrappableInputFormatInterface;
     boolean isCacheOnly = inputFormat instanceof LlapCacheOnlyInputFormatInterface;
@@ -491,7 +497,7 @@ public class HiveInputFormat<K extends WritableComparable, V extends Writable>
      * false due to an invalid cache key (like: "null_limit_reached"), so this will silently acts as
      * limit hasn't been reached, which is a proper behavior in case we don't support early bailout.
      */
-    return LimitOperator.checkLimitReachedForVertex(job, job.get("tez.mapreduce.vertex.name"));
+    return LimitOperator.checkLimitReachedForVertex(job);
   }
 
   protected void init(JobConf job) {
