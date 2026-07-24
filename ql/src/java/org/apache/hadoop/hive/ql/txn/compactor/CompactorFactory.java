991e80af7eb05429c41433b6c8e2c0dd0c6c7b3b
mr3project <59910162+mr3project@users.noreply.github.com>
2026-07-24 17:33:48 +0900

    Compactor: require Tez for full ACID compaction

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/CompactorFactory.java
@@ -72,18 +72,19 @@ public final class CompactorFactory {
   public CompactorPipeline getCompactorPipeline(Table table, HiveConf configuration, CompactionInfo compactionInfo, IMetaStoreClient msc)
       throws HiveException {
     if (AcidUtils.isFullAcidTable(table.getParameters())) {
-      boolean tezExecution = "tez".equalsIgnoreCase(
-          HiveConf.getVar(configuration, HiveConf.ConfVars.HIVE_EXECUTION_ENGINE));
+      if (!"tez".equalsIgnoreCase(HiveConf.getVar(configuration, HiveConf.ConfVars.HIVE_EXECUTION_ENGINE))) {
+        throw new HiveException("Compaction is only supported with Tez execution engine.");
+      }
       boolean queryBased = HiveConf.getBoolVar(configuration, HiveConf.ConfVars.COMPACTOR_CRUD_QUERY_BASED);
       boolean mergeEnabled = HiveConf.getBoolVar(configuration, HiveConf.ConfVars.HIVE_MERGE_COMPACTION_ENABLED);
-      if (!tezExecution || (!queryBased && !mergeEnabled)) {
-        if (CompactionType.REBALANCE.equals(compactionInfo.type)) {
+      if (CompactionType.REBALANCE.equals(compactionInfo.type)) {
+        if (!queryBased) {
           throw new HiveException("Rebalancing compaction is only supported in Tez, and via Query based compaction. " +
               "Set hive.execution.engine=tez and hive.compactor.crud.query.based=true to enable it.");
         }
-        if (!tezExecution && queryBased) {
-          LOG.warn("Query-based compaction is enabled, but it is only supported on tez. Falling back to MR compaction.");
-        }
+        return new CompactorPipeline(new RebalanceQueryCompactor());
+      }
+      if (!queryBased && !mergeEnabled) {
         return new CompactorPipeline(new MRCompactor(msc));
       }
       switch (compactionInfo.type) {
@@ -93,12 +94,6 @@ public final class CompactorFactory {
         case MAJOR:
           return new CompactorPipeline(new MergeCompactor())
                   .addCompactor(queryBased ? new MajorQueryCompactor() : new MRCompactor(msc));
-        case REBALANCE:
-          if (!queryBased) {
-            throw new HiveException("Rebalancing compaction is only supported in Tez, and via Query based compaction. " +
-                "Set hive.execution.engine=tez and hive.compactor.crud.query.based=true to enable it.");
-          }
-          return new CompactorPipeline(new RebalanceQueryCompactor());
       }
     } else if (AcidUtils.isInsertOnlyTable(table.getParameters())) {
       if (!configuration.getBoolVar(HiveConf.ConfVars.HIVE_COMPACTOR_COMPACT_MM)) {
